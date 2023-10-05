d14fa8705a04de23ca6bc1385d22909f5491d617
Sama Moayeri <smoy99@gmail.com>
2023-10-05 20:25:41 +0330

    Oct 5-3

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -26,63 +26,22 @@ export default function Book() {
                 minHeight={350.35}
                 minWidth={250}
                 usePortrait={true}
-                // mobileScrollSupport={true}
+                mobileScrollSupport={true}
 
             >
-                <div className="bg-black shadow-black shadow-lg aspect-A4 rounded-2xl">
+                <div className="bg-black shadow-black shadow-lg aspect-A4">
                     <Cover/>
                 </div>
-                <div className="bg-black shadow-gray-800 shadow-md">
-                    <img src={'/portfolioImages/A4 - 2.jpg'} className={'absolute'}/>
-                    <div
-                    //     className={
-                    //     'left-page'
-                    // }
-                    ></div>
+                <div className="bg-black shadow-black shadow-lg aspect-A4">
+                    <Cover/>
                 </div>
-                <div className="demoPage bg-slate-600">
-                    <img src={'/portfolioImages/A4 - 3.jpg'} className={'absolute'}/>
+                <div className="bg-black shadow-black shadow-lg aspect-A4">
+                    <Cover/>
+                </div>
+                <div className="bg-black shadow-black shadow-lg aspect-A4">
+                    <Cover/>
+                </div>
 
-                </div>
-                <div className="demoPage bg-red-300">
-                    <img src={'/portfolioImages/A4 - 16.jpg'} className={'absolute'}/>
-                    <div className={
-                        'left-page'
-                    }></div>
-                </div>
-                <div className="demoPage bg-lime-300">
-                    <img src={'/portfolioImages/A4 - 17.png'}/>
-
-                </div>
-                <div className="demoPage bg-lime-400">
-                    <img src={'/portfolioImages/A4 - 34.png'} className={'absolute'}/>
-                    <div className={
-                        'left-page'
-                    }></div>
-
-                </div>
-                <div className="demoPage bg-slate-600">
-                    <img src={'/portfolioImages/A4 - 35.png'}/>
-
-                </div>
-                <div className="demoPage bg-red-300">
-                    <ReactPlayer
-                        url={'/portfolioImages/Last Part.mp4'}
-                        muted={true}
-                        controls={true}
-                        width="530px"
-                    >
-                    </ReactPlayer>
-                    {/*< video src={'/portfolioImages/Last Part.mp4'} autoPlay={true} className={'object-cover'} controls={true}/>*/}
-
-                </div>
-                <div className="demoPage bg-lime-300">
-                    <img src={'/portfolioImages/A4 - 39.png'}/>
-
-                </div>
-                <div className="demoPage bg-lime-400">Page 3</div>
-                <div className="demoPage bg-slate-600">Page 4</div>
-                <div className="demoPage bg-slate-600">Page 4</div>
             </HTMLFlipBook>
         </div>
     );
