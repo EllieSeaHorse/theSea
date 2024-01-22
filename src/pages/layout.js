3a4a60d763166a026964d0fbddb0c24ebf40c971
Sama Moayeri <smoy99@gmail.com>
2024-01-22 13:56:34 +0330

    testjan22

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -17,6 +17,7 @@ export default function Layout({ children }) {
             >
                 {children}
                 <Analytics/>
+                <SpeedInsights/>
             </div>
         </div>
     );
